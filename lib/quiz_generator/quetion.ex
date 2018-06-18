058b0acac3829a55caf350d2dd388d8e53555465
peelle <developer@peelle.org>
2018-06-18 23:47:33 +0800

    added FIB section

--- lib/quiz_generator/quetion.ex
+++ lib/quiz_generator/quetion.ex
@@ -1,9 +1,8 @@
 defmodule QuizGenerator.Question do
-  alias __MODULE__
-  alias QuizGenerator.QA
   alias QuizGenerator.InputList
 
-  # Adding the answer....
+
+  # Multiple Choice.
   def get_multiple_choices( %InputList{ QA: qa_mapset }, answer, _options) do
     MapSet.to_list(qa_mapset)
     |> Enum.take_random(5) # When enabling options, takes whatever they would specify + 1. ignoring the edge case that they would specify as much as or more more than exits.
@@ -18,8 +17,14 @@ defmodule QuizGenerator.Question do
     { :error, :must_use_InputList_struct }
   end
 
-  def generate_questions(input_list, _options) do
+  def generate_multiple_choice_questions(input_list, _options) do
     for qa <- input_list."QA", do: %{ qa.question => get_multiple_choices(input_list, qa.answer, "") }
+  end
+
+
+  # Fill In The Blank
+  def generate_fib_questions(input_list, _options) do
+    for qa <- input_list."QA", do: %{ qa.question => "_______" }
   end
 
 end
