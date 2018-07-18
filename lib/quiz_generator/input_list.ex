afdcd3bce44d13bc6198d2232b1e730d81688b70
peelle <developer@peelle.org>
2018-07-19 00:12:12 +0800

    Added 3 way and some helper functions.

--- lib/quiz_generator/input_list.ex
+++ lib/quiz_generator/input_list.ex
@@ -10,8 +10,17 @@ defmodule QuizGenerator.InputList do
     update_in(list.q_and_a, &MapSet.put(&1, %{ question: question, answer: answer })) 
   end
 
+  def insert(%InputList{} = list, question, answer, extra_info) do
+    update_in(list.q_and_a, &MapSet.put(&1, %{ question: question, answer: answer, extra_info: extra_info })) 
+  end
+
   def get_column(%InputList{ q_and_a: list}, column_name) do
     MapSet.to_list(list)
+    |> Enum.map( fn(item) -> item[column_name] end )
+  end
+
+  def get_column_from_list( list, column_name) do
+    list
     |> Enum.map( fn(item) -> item[column_name] end )
   end
 
@@ -38,6 +47,20 @@ defmodule QuizGenerator.InputList do
   Question.generate(:fill_in_blank, list, %{ max_questions: 7, max_choices: nil})
   Question.generate(:matching, list)
 
+    list = InputList.new()
+
+    list = InputList.insert(list, "我", "I; Me", "wo" ) 
+    list = InputList.insert(list, "漂", "Drift; Float", "piao" ) 
+    list = InputList.insert(list, "没", "Not have; Is not; be without", "mei" ) 
+    list = InputList.insert(list, "向", "Direction", "xiang" ) 
+    list = InputList.insert(list, "有", "Have; Possess, own; exist; contain", "you" ) 
+    list = InputList.insert(list, "北方", "North; Northern place", "beifang" ) 
+    list = InputList.insert(list, "忍耐", "Patience", "rennai" ) 
+    list = InputList.insert(list, "别", "Don't; Other", "bie" ) 
+    list = InputList.insert(list, "问", "Ask", "wen" ) 
+    list = InputList.insert(list, "家乡", "Hometown; Homeland", "jia1xiang" ) 
+
+    Question.generate(:three_way, list)
   """
 
 end
--- lib/quiz_generator/quetion.ex
+++ lib/quiz_generator/quetion.ex
@@ -1,29 +1,40 @@
 defmodule QuizGenerator.Question do
   alias QuizGenerator.InputList
 
-  @default_options %{ max_questions: nil, max_choices: 4 }
+  @default_options %{ max_questions: nil, max_choices: 3 }
 
   def generate(a, b, options \\ @default_options ) # This is a function template.
-  def generate(:multiple_choice, list, %{ max_questions: max_questions, max_choices: max_choices } ) do
-    {:ok, (for qa <- list."q_and_a", do: %{ qa.question => generate_multiple_choices(list, qa.answer, max_choices) }) |> Enum.take_random(max_questions) }
+
+  def generate(:multiple_choice, list, options ) do
+    {:ok, (for qa <- list."q_and_a" do 
+      %{ qa.question => generate_multiple_choices(list, qa.answer, options.max_choices) }
+    end ) 
+    |> Enum.take_random(options.max_questions || Enum.count(list.q_and_a)) 
+    }
   end
 
   # Fill In The Blank
-  def generate(:fill_in_blank, list, %{ max_questions: max_questions, max_choices: _ }) do
+  def generate(:fill_in_blank, list, options) do
     { :ok, 
-      (for qa <- list."q_and_a", do: %{ qa.question => "_______" }) |> Enum.take_random(max_questions)
+      (for qa <- list."q_and_a", do: %{ qa.question => "_______" }) |> Enum.take_random(options.max_questions || Enum.count(list.q_and_a))
     }
   end
 
-  def generate(:matching, list,  %{ max_questions: max_questions, max_choices: _ }) do
+  def generate(:matching, list,  options) do
     {:ok, 
-      InputList.get_column(list,:question)
-      |> Enum.take(max_questions)
-      |> Enum.zip(generate_matching(list, ""))
+      InputList.get_column(list, :question)
+      |> Enum.take(options.max_questions)
+      |> Enum.zip(generate_matching(list, options))
     }
   end
-    
-  #def generate(_, _), do: { :error, "Unknown section type." }
+
+  def generate(:three_way, list, options) do
+      sub_list = list.q_and_a |> Enum.take(options.max_questions || Enum.count(list.q_and_a))
+    tuple = Enum.zip([ Enum.shuffle(InputList.get_column_from_list(sub_list, :question)), Enum.shuffle(InputList.get_column_from_list(sub_list, :answer)), Enum.shuffle(InputList.get_column_from_list(sub_list, :extra_info))])
+    {:ok, tuple}
+  end
+
+  # Default catchall
   def generate(_, _, _), do: { :error, "Unknown section type." }
 
   # Multiple Choice.
@@ -38,8 +49,7 @@ defmodule QuizGenerator.Question do
 
   defp generate_matching( %InputList{} = list, options) do
     InputList.get_column(list, :answer)
-    |> Enum.take(options.max_questions)
+    |> Enum.take(options.max_questions || Enum.count(list.q_and_a))
     |> Enum.shuffle
   end
-
 end
