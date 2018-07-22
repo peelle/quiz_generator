4ff0cf0d1cd6c041575076110ae7034f818624c5
peelle <developer@peelle.org>
2018-07-23 00:55:38 +0800

    Added an answer struct

--- lib/quiz_generator/quetion.ex
+++ lib/quiz_generator/quetion.ex
@@ -1,14 +1,14 @@
 defmodule QuizGenerator.Question do
   alias QuizGenerator.InputList
 
-  @default_options %{ max_questions: nil, max_choices: 3 }
+  @default_options %{ max_questions: nil, max_choices: 3, answer_key: false }
 
   def generate(a, b, options \\ @default_options ) # This is a function template.
 
   def generate(:multiple_choice, list, options ) do
     {:ok, (for qa <- list."q_and_a" do 
-      %{ qa.question => generate_multiple_choices(list, qa.answer, options.max_choices) }
-    end ) 
+            %{ qa.question => generate_multiple_choices(list, qa.answer, options.max_choices) }
+          end ) 
     |> Enum.take_random(options.max_questions || Enum.count(list.q_and_a)) 
     }
   end
@@ -37,6 +37,16 @@ defmodule QuizGenerator.Question do
   # Default catchall
   def generate(_, _, _), do: { :error, "Unknown section type." }
 
+  def answer_key(questions, list) do
+    # I had a tough time figuring this out. 
+    # Might be beacuse I picked the wrong DS. 
+    for q <- questions,
+        question <- MapSet.to_list(list.q_and_a), 
+        List.first(Map.keys(q)) == Map.get(question, :question) do 
+      { List.first(Map.keys(q)), question.answer }
+    end
+  end
+
   # Multiple Choice.
   defp generate_multiple_choices( %InputList{} = list, answer, max_choices) do
     InputList.get_column(list, :answer)
@@ -52,4 +62,5 @@ defmodule QuizGenerator.Question do
     |> Enum.take(options.max_questions || Enum.count(list.q_and_a))
     |> Enum.shuffle
   end
+
 end
