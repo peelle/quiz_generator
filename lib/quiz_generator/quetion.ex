094b3441733ade1d26fedb730216d3b2e73332b6
peelle <developer@peelle.org>
2018-08-26 22:59:45 +0800

    fixed a bug, and the made return DS match for all types of questions.

--- lib/quiz_generator/quetion.ex
+++ lib/quiz_generator/quetion.ex
@@ -1,13 +1,13 @@
 defmodule QuizGenerator.Question do
   alias QuizGenerator.InputList
 
-  @default_options %{ max_questions: nil, max_choices: 3 }
+  @default_options %{ max_questions: nil, max_choices: 4 }
 
   def generate(a, b, options \\ @default_options ) # This is a function template.
 
   def generate(:multiple_choice, list, options ) do
     {:ok, 
-      (for qa <- list."q_and_a", do: %{ qa.question => generate_multiple_choices(list, qa.answer, options.max_choices) } )
+      (for qa <- list.q_and_a, do: { qa.question, generate_multiple_choices(list, qa.answer, options.max_choices) } )
       |> Enum.take_random(options.max_questions || Enum.count(list.q_and_a)) 
     }
   end
@@ -15,7 +15,7 @@ defmodule QuizGenerator.Question do
   # Fill In The Blank
   def generate(:fill_in_blank, list, options) do
     { :ok, 
-      (for qa <- list."q_and_a", do: %{ qa.question => "_______" }) 
+      (for qa <- list.q_and_a, do: { qa.question, "_______" }) 
       |> Enum.take_random(options.max_questions || Enum.count(list.q_and_a))
     }
   end
@@ -50,10 +50,9 @@ defmodule QuizGenerator.Question do
   # Multiple Choice.
   defp generate_multiple_choices( %InputList{} = list, answer, max_choices) do
     InputList.get_column(list, :answer)
-    |> Enum.take_random(max_choices) # When enabling options, takes whatever they would specify + 1. ignoring the edge case that they would specify as much as or more more than exits.
-    |> Stream.into([answer]) # am not sure if this has any sort of order guarentee
-    |> Stream.uniq
-    |> Stream.take(max_choices) # TODO: replace with optional/default mpc answer #
+    |> Stream.reject(&(answer == &1))
+    |> Enum.take_random(max_choices-1) # When enabling options, takes whatever they would specify + 1. ignoring the edge case that they would specify as much as or more more than exits.
+    |> Enum.into([answer]) # am not sure if this has any sort of order guarentee
     |> Enum.shuffle
   end
 
